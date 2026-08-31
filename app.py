17d86f0e1b15042843f5ec1c6046205c10b76b62
sascha2115 <sascha2115@users.noreply.github.com>
2026-08-31 12:27:27 +0200

    chore: bump version to 1.1.21

--- app.py
+++ app.py
@@ -13,7 +13,7 @@
 # python3 app.py --simulate-download-error
 # ==================================================================================================================================
 #
-APP_VERSION = "1.1.20"
+APP_VERSION = "1.1.21"
 import gc
 import glob
 import html
